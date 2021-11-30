5821ec4c80746c81f3523157a4a600fe180883d1
Alex <pandiyanalex003@gmail.com>
2021-11-30 12:12:25 +0530

    training-task

---
+++ check.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>hj</title>
+</head>
+<body>
+
+    <form></form>
+
+    <?php
+    
+    include ('test.php');
+    
+    ?>
+
+</body>
+</html>
